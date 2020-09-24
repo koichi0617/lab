c06be3697d0effec208105d1947fe6911abced8b
koichi0617 <koichi0617@i.softbank.jp>
2020-09-24 18:29:54 +0900

    write dictionary

--- get.py
+++ get.py
@@ -1,12 +1,17 @@
-# for num in range(10):
-#   push(num, aaa, 1)
 
-def push(state, value):
+states = {'aaa': 1, 'bbb': 2, 'ccc': 3}
+
+def push(states):
   with open('output.txt', mode='a') as f:
-    f.write(state)
-    for num in range(24-len(state)):
-      f.write(' ')
-    f.write(value)
+    keys = states.keys()
+    values = states.values()
+    for i in range(len(states)):
+      f.write(keys[i])
+      for num in range(24-len(keys[i])):
+        f.write(' ')
+      f.write(str(values[i]))
+      f.write('\n')
 
-
-push('aaa', '1')
+with open('output.txt', mode='a') as f:
+  
+  push(states)
