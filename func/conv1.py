2958a1c680a0df486a76f09daa6ed5a8061d27a4
koichi0617 <koichi0617@i.softbank.jp>
2020-10-04 14:49:52 +0900

    conv1

--- func/conv1.py
+++ func/conv1.py
@@ -3,15 +3,18 @@ def conv1(pattern):
     initial = f.read()
   with open(file, mode='w') as f:
     f.write(initial)
-  with open(file, mode='a') as f:
-    if pattern == 'A':
-      x_send(A[0], A[1], A[2])
-      x_calc(A[3], A[4], A[5])
-      x_back(A[6], A[7], A[8])
-    else:
-      x_send(B[0], B[1], B[2])
-      x_calc(B[3], B[4], B[5])
-      x_back(B[6], B[7], B[8])
+  with open(file, mode='r') as f:
+    list = f.readlines()
+    all = len(list)
+    x_send(all, pattern[0], pattern[1], pattern[2])
+  with open(file, mode='r') as f:
+    list = f.readlines()
+    all = len(list)
+    x_calc(all, pattern[3], pattern[4], pattern[5])
+  with open(file, mode='r') as f:
+    list = f.readlines()
+    all = len(list)
+    x_back(all, pattern[6], pattern[7], pattern[8])
   with open(file, mode='r') as f:
     list = f.readlines()
     i = 0
@@ -24,114 +27,143 @@ def conv1(pattern):
   with open(file, mode='w') as f:
     f.writelines(list)
 
-def x_send(addr, loop, row): #(SRAM開始アドレス、ループ回数、挿入開始列)
+def x_send(num, addr, loop, row): #(SRAM開始アドレス、ループ回数、挿入開始列)
   with open(file, mode='r') as f:
     list = f.readlines()
     # #0の末尾は最後の命令から２回改行
-    list.insert(-1, '\n')
-    list.insert(-1, 'send')
-    list.insert(-1, '#1\n')
-    list.insert(-1, push('INST_RADDRX', addr, 16))
-    list.insert(-1, push('INST_RCEBX', 0, 1))
-    list.insert(-1, '\n')
-    list.insert(-1, '#2\n')
-    list.insert(-1, push('INST_WBUF_EN', 1, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', row, 6))
-    list.insert(-1, '\n')
-    list.insert(-1, '#3\n')
-    list.insert(-1, push('INST_COUNTER0_WE', 1, 1))
-    list.insert(-1, push('INST_COUNTER0', loop, 16))
-    list.insert(-1, '\n')
-    list.insert(-1, '#4\n')
-    list.insert(-1, push('INST_COUNTER0_WE', 0, 1))
-    list.insert(-1, push('INST_JUMP_COUNTER0', 1, 1))
-    list.insert(-1, push('INST_PC', 2, 32))
-    list.insert(-1, '\n')
-    list.insert(-1, '#5\n')
-    list.insert(-1, push('INST_JUMP_COUNTER0', 0, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', 1, 6))
-    list.insert(-1, '\n')
-    list.insert(-1, '#6\n')
-    list.insert(-1, push('INST_WBUF_EN', 0, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', 0, 6))
-    list.insert(-1, push('INST_RADDRX', -1, 16))
-    list.insert(-1, '\n')
-    list.insert(-1, '#7\n')
-    list.insert(-1, push('INST_RADDRX', 0, 16))
+    list.insert(num, '\n')
+    list.insert(num, '//send\n')
+    list.insert(num, '#1\n')
+    list.insert(num, push('INST_RADDRX', addr, 16))
+    list.insert(num, push('INST_RCEBX', 0, 1))
+    list.insert(num, '\n')
+    list.insert(num, '#2\n')
+    list.insert(num, push('INST_WBUF_EN', 1, 1))
+    list.insert(num, push('INST_WBUF_EN_CTRL', row, 6))
+    list.insert(num, '\n')
+    list.insert(num, '#3\n')
+    list.insert(num, push('INST_COUNTER0_WE', 1, 1))
+    list.insert(num, push('INST_COUNTER0', loop, 16))
+    list.insert(num, '\n')
+    list.insert(num, '#4\n')
+    list.insert(num, push('INST_COUNTER0_WE', 0, 1))
+    list.insert(num, push('INST_JUMP_COUNTER0', 1, 1))
+    list.insert(num, push('INST_PC', 2, 32))
+    list.insert(num, '\n')
+    list.insert(num, '#5\n')
+    list.insert(num, push('INST_JUMP_COUNTER0', 0, 1))
+    list.insert(num, push('INST_WBUF_EN_CTRL', 1, 6))
+    list.insert(num, '\n')
+    list.insert(num, '#6\n')
+    list.insert(num, push('INST_WBUF_EN', 0, 1))
+    list.insert(num, push('INST_WBUF_EN_CTRL', 0, 6))
+    list.insert(num, push('INST_RADDRX', -1, 16))
+    list.insert(num, '\n')
+    list.insert(num, '#7\n')
+    list.insert(num, push('INST_RADDRX', 0, 16))
     with open(file, mode='w') as f:
       f.writelines(list)
 
-def x_calc(addr, loop, row): #(SRAM開始アドレス、ループ回数、挿入開始列)
+def x_calc(num, addr, loop, row): #(SRAM開始アドレス、ループ回数、挿入開始列)
   with open(file, mode='r') as f:
     list = f.readlines()
     # #0の末尾は最後の命令から２回改行
-    list.insert(-1, '\n')
-    list.insert(-1, '//calc')
-    list.insert(-1, '#1\n')
-    list.insert(-1, push('INST_RADDRX', addr, 16))
-    list.insert(-1, push('INST_RCEBX', 0, 1))
-    list.insert(-1, '\n')
-    list.insert(-1, '#2\n')
-    list.insert(-1, push('INST_WBUF_EN', 1, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', row, 6))
-    list.insert(-1, '\n')
-    list.insert(-1, '#3\n')
-    list.insert(-1, push('INST_COUNTER0_WE', 1, 1))
-    list.insert(-1, push('INST_COUNTER0', loop, 16))
-    list.insert(-1, '\n')
-    list.insert(-1, '#4\n')
-    list.insert(-1, push('INST_COUNTER0_WE', 0, 1))
-    list.insert(-1, push('INST_JUMP_COUNTER0', 1, 1))
-    list.insert(-1, push('INST_PC', 2, 32))
-    list.insert(-1, '\n')
-    list.insert(-1, '#5\n')
-    list.insert(-1, push('INST_JUMP_COUNTER0', 0, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', 1, 6))
-    list.insert(-1, '\n')
-    list.insert(-1, '#6\n')
-    list.insert(-1, push('INST_WBUF_EN', 0, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', 0, 6))
-    list.insert(-1, push('INST_RADDRX', -1, 16))
-    list.insert(-1, '\n')
-    list.insert(-1, '#7\n')
-    list.insert(-1, push('INST_RADDRX', 0, 16))
+    list.insert(num, '\n')
+    list.insert(num, '//calc\n')
+    list.insert(num, '#1\n')
+    list.insert(num, push('INST_RADDRX', addr, 16))
+    list.insert(num, push('INST_RCEBX', 0, 1))
+    list.insert(num, '\n')
+    list.insert(num, '#2\n')
+    list.insert(num, push('INST_WBUF_EN', 1, 1))
+    list.insert(num, push('INST_WBUF_EN_CTRL', row, 6))
+    list.insert(num, '\n')
+    list.insert(num, '#3\n')
+    list.insert(num, push('INST_COUNTER0_WE', 1, 1))
+    list.insert(num, push('INST_COUNTER0', loop, 16))
+    list.insert(num, '\n')
+    list.insert(num, '#4\n')
+    list.insert(num, push('INST_COUNTER0_WE', 0, 1))
+    list.insert(num, push('INST_JUMP_COUNTER0', 1, 1))
+    list.insert(num, push('INST_PC', 2, 32))
+    list.insert(num, '\n')
+    list.insert(num, '#5\n')
+    list.insert(num, push('INST_JUMP_COUNTER0', 0, 1))
+    list.insert(num, push('INST_WBUF_EN_CTRL', 1, 6))
+    list.insert(num, '\n')
+    list.insert(num, '#6\n')
+    list.insert(num, push('INST_WBUF_EN', 0, 1))
+    list.insert(num, push('INST_WBUF_EN_CTRL', 0, 6))
+    list.insert(num, push('INST_RADDRX', -1, 16))
+    list.insert(num, '\n')
+    list.insert(num, '#7\n')
+    list.insert(num, push('INST_RADDRX', 0, 16))
     with open(file, mode='w') as f:
       f.writelines(list)
 
-def x_back(addr, loop, row): #(SRAM開始アドレス、ループ回数、挿入開始列)
-  with open(file, mode='r') as f:
-    list = f.readlines()
+def x_back(num, addr, loop, row): #(SRAM開始アドレス、ループ回数、挿入開始列)
+  with open(file, mode='r') as l:
+    list = l.readlines()
     # #0の末尾は最後の命令から２回改行
-    list.insert(-1, '\n')
-    list.insert(-1, '//back')
-    list.insert(-1, '#1\n')
-    list.insert(-1, push('INST_RADDRX', addr, 16))
-    list.insert(-1, push('INST_RCEBX', 0, 1))
-    list.insert(-1, '\n')
-    list.insert(-1, '#2\n')
-    list.insert(-1, push('INST_WBUF_EN', 1, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', row, 6))
-    list.insert(-1, '\n')
-    list.insert(-1, '#3\n')
-    list.insert(-1, push('INST_COUNTER0_WE', 1, 1))
-    list.insert(-1, push('INST_COUNTER0', loop, 16))
-    list.insert(-1, '\n')
-    list.insert(-1, '#4\n')
-    list.insert(-1, push('INST_COUNTER0_WE', 0, 1))
-    list.insert(-1, push('INST_JUMP_COUNTER0', 1, 1))
-    list.insert(-1, push('INST_PC', 2, 32))
-    list.insert(-1, '\n')
-    list.insert(-1, '#5\n')
-    list.insert(-1, push('INST_JUMP_COUNTER0', 0, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', 1, 6))
-    list.insert(-1, '\n')
-    list.insert(-1, '#6\n')
-    list.insert(-1, push('INST_WBUF_EN', 0, 1))
-    list.insert(-1, push('INST_WBUF_EN_CTRL', 0, 6))
-    list.insert(-1, push('INST_RADDRX', -1, 16))
-    list.insert(-1, '\n')
-    list.insert(-1, '#7\n')
-    list.insert(-1, push('INST_RADDRX', 0, 16))
+    list.insert(num, '\n')
+    num += 1
+    list.insert(num, '//back\n')
+    num += 1
+    list.insert(num, '#1\n')
+    num += 1
+    list.insert(num, push('INST_RADDRX', addr, 16))
+    num += 1
+    list.insert(num, push('INST_RCEBX', 0, 1))
+    num += 1
+    list.insert(num, '\n')
+    num += 1
+    list.insert(num, '#2\n')
+    num += 1
+    list.insert(num, push('INST_WBUF_EN', 1, 1))
+    num += 1
+    list.insert(num, push('INST_WBUF_EN_CTRL', row, 6))
+    num += 1
+    list.insert(num, '\n')
+    num += 1
+    list.insert(num, '#3\n')
+    num += 1
+    list.insert(num, push('INST_COUNTER0_WE', 1, 1))
+    num += 1
+    list.insert(num, push('INST_COUNTER0', loop, 16))
+    num += 1
+    list.insert(num, '\n')
+    num += 1
+    list.insert(num, '#4\n')
+    num += 1
+    list.insert(num, push('INST_COUNTER0_WE', 0, 1))
+    num += 1
+    list.insert(num, push('INST_JUMP_COUNTER0', 1, 1))
+    num += 1
+    list.insert(num, push('INST_PC', 2, 32))
+    num += 1
+    list.insert(num, '\n')
+    num += 1
+    list.insert(num, '#5\n')
+    num += 1
+    list.insert(num, push('INST_JUMP_COUNTER0', 0, 1))
+    num += 1
+    list.insert(num, push('INST_WBUF_EN_CTRL', 1, 6))
+    num += 1
+    list.insert(num, '\n')
+    num += 1
+    list.insert(num, '#6\n')
+    num += 1
+    list.insert(num, push('INST_WBUF_EN', 0, 1))
+    num += 1
+    list.insert(num, push('INST_WBUF_EN_CTRL', 0, 6))
+    num += 1
+    list.insert(num, push('INST_RADDRX', -1, 16))
+    num += 1
+    list.insert(num, '\n')
+    num += 1
+    list.insert(num, '#7\n')
+    num += 1
+    list.insert(num, push('INST_RADDRX', 0, 16))
     with open(file, mode='w') as f:
       f.writelines(list)
 
@@ -151,7 +183,32 @@ def push(state, value, width):
   return state + space + binary +'\n'
 
 
+#命令のまとまりを指定位置に挿入する関数
+def set_command(num, command, pattern): #挿入位置（#num以降に挿入）、挿入する命令のまとまり
+  target = '#%d\n' % num
+  rows = 0
+  i = 0
+  with open(file, mode='r') as f:
+    list = f.readlines()
+    for line in list:
+      rows = rows + 1
+      if '#' in line: 
+        if line == target:
+          command(rows-3, pattern[0], pattern[1], pattern[2])
+  rows = 0
+  with open(file, mode='r') as f:
+    list = f.readlines()
+    for line in list:
+      rows = rows + 1
+      if '#' in line: 
+        list[rows-1] = '#%d\n' % i
+        i = i + 1
+  with open(file, mode='w') as f:
+    f.writelines(list)
+
 A = [1, 32, 1, 1, 32, 1, 1, 32, 1]
 B = [16, 16, 1, 16, 16, 1, 16, 16, 1]
 file = 'output.txt'
-conv1('B')
+#conv1(B)
+
+set_command(8, x_back, A)
